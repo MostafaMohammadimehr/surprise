7b8a31cfdb11722bb36c5ef2156d244e8018a57b
Mostafa <m.mohammadimehr1390@gmail.com>
2025-09-03 17:17:32 +0330

    postal1

---
+++ assets/js/postal-card.js
@@ -0,0 +1,4 @@
+document.getElementById("openBtn").addEventListener("click", function () {
+  const frame = document.getElementById("cardFrame");
+  frame.src = "https://digipostal.ir/c4rrz8m"; // لینک کارت پستالت
+});
